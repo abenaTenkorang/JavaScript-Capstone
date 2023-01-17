2d3e1a266605e2221456b29cdd219f180d6590b6
abenaTenkorang <abenagy16@gmail.com>
2023-01-17 13:06:58 +0000

    Add function for counting item

---
+++ modules/countMovie.js
@@ -0,0 +1,12 @@
+import getData from './getdata.js';
+
+const countedMovies = document.querySelector('.movie-no');
+
+const count = async () => {
+  const moviesNumber = await getData();
+  const total = moviesNumber.length;
+  countedMovies.textContent = `Movies(${total})`;
+  return total;
+};
+
+count();
