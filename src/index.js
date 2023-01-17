86bc60666610bca588940186941e94698dfcb588
abenaTenkorang <abenagy16@gmail.com>
2023-01-17 11:33:12 +0000

    Run build

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import './style.css';
+﻿import './style.css';
+import getData from '../modules/getdata.js';
+import showMovies from '../modules/showMovies.js';
+
+document.addEventListener('DOMContentLoaded', showMovies);
+
+const displayItem = async () => {
+  const movies = await getData();
+  showMovies(movies);
+};
+displayItem();
