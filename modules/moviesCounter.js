c00ac291a82a8f1061fd9f337e8c1120857b4e40
abenaTenkorang <abenagy16@gmail.com>
2023-01-18 20:49:28 +0000

    Test for comment count

---
+++ modules/moviesCounter.js
@@ -0,0 +1,2 @@
+﻿const countComments = (response) => (typeof (response) === 'object' ? response.length : 'no match');
+export default countComments;
--- modules/moviesCounter.test.js
+++ modules/moviesCounter.test.js
@@ -1,49 +1,35 @@
-/**
- * @jest-environment jsdom
- */
-import { count } from "./countMovie";
-// import 'whatwg-fetch'
-import "isomorphic-fetch"
+import countComments from './moviesCounter.js';
 
-document.body.innerHTML = `<div class="main-container">
-<div class="nav">
-  <div class="nav-img"></div>
-  <div class="search-tag">
-    <form class="search" id="" action="">
-      <input class="search-input" type="text" name="search" placeholder="Search">
-      <button class="btn-srch" type="submit">
-        <i class="fa fa-search" aria-hidden="true"></i>
-      </button>
-    </form>
-  </div>
-  <div class="nav-m">
-    <ul class="nav-items">
-      <li><a href="#" class="movie-no">Movie(0)</a></li>
-      <li><a href="#">Series</a></li>
-      <li><a href="#">Soaps</a></li>
-    </ul>
-  </div>
-</div> 
-<main class="main-js-page">
-  <!-- js files goes here -->
-</main>
-<div class="popup">
-  <!-- js files goes here -->
-</div>
-<footer>
-  <div class="foot-left">
-    <p>All Rights Reserved</p>
-  </div>
-  <div class="foot-right">
-    <p>Made with <i class="fa-solid fa-heart"></i> by Abi And Monica</p>
-  </div>
-</footer>
-</div>`
+test('count the number of comments are return number', () => {
+  const testData = [
+    {
+      item_id: 1,
+      username: 'monica',
+      comment: 'loved it',
+    },
+    {
+      item_id: 2,
+      username: 'abi',
+      comment: 'good Movie',
 
-describe('Test movies counter on homepage', ()=> {
-    test('Movies(240)', ()=> {
-        const movieCounter = document.querySelector('.movie-no');
-        count(240)
-        expect(movieCounter.innerText).toEqual('Movies(240)')
-    })
-})
+    },
+    {
+      item_id: 3,
+      username: 'abi',
+      comment: 'enjoyed it',
+
+    },
+  ];
+  const response = countComments(testData);
+  expect(response).toBe(3);
+});
+test('properlly displays the error', () => {
+  const testData = 'invalid';
+  const response = countComments(testData);
+  expect(response).toBe('no match');
+});
+test('properlly counts the number of comments for an array', () => {
+  const testData = ['a', 'b', 'c'];
+  const response = countComments(testData);
+  expect(response).toBe(3);
+});
