8228e4de6341b386b434c6c2b193599f080219b5
monicah79 <monniewanjema@gmail.com>
2023-01-18 07:08:22 +0300

    Fix eslinters

--- modules/counters.test.js
+++ modules/counters.test.js
@@ -1,8 +1,9 @@
-import countComments from "./countComment";
+import countComments from './countComment.js';
+
 test("[cont1, cont2, cont3, cont4] expect length to be '4'", () => {
-    const counts = ['cont1', 'cont2', 'cont3', 'cont4'];
-    const emptyCount = countComments([]);
-    const result = countComments(counts);
-    expect(emptyCount).toBe(0);
-    expect(result).toBe(4);
-  });
+  const counts = ['cont1', 'cont2', 'cont3', 'cont4'];
+  const emptyCount = countComments([]);
+  const result = countComments(counts);
+  expect(emptyCount).toBe(0);
+  expect(result).toBe(4);
+});
